419771864a1870ae0641df7d64403503ff349960
chamalj1980 <64780912+chamalj1980@users.noreply.github.com>
2023-08-07 10:37:33 +0530

    Update ChatGPT_Quiz_App.py

--- QuizBot/ChatGPT_Quiz_App.py
+++ QuizBot/ChatGPT_Quiz_App.py
@@ -1,12 +1,13 @@
-# Demo App for the course Building AI Applications with ChatGPT and OpenAI LLMs
-# by Sumudu Tennakoon
-# Last Update: 2022-07-09
-# streamlit run .\GitHub\BuildingAIApplicationsWithChatGPT-main\DemoApp\ChatGPT_Demo_App.py
+# A chatGPT (Open AI APIs) based quiz demo application as the capstone project for the course Introduction to AI Application Development using ChatGPT by
+Dr. Sumudu Thennakoon 
+# by Chamila Wijayawardhna
+# Last Update: 2023-08-07
+# streamlit run .\GitHub\ChatGPTQuizBot-main\QuizBot\ChatGPT_Quiz_App.py
 
 import streamlit as st
 
-st.header("Building AI Applications with ChatGPT - OpenAI Demo App")
-st.subheader("Sumudu Tennakoon")
+st.header("Building AI Applications with ChatGPT - OpenAI Demo Quiz App")
+st.subheader("Chamila Wijayawardhana")
 st.write("https://github.com/chamalj1980/ChatGPTQuizBot/tree/main/QuizBotT")
 
 
